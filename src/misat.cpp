5c9b2e9b0f23306f903d50eebd4b6236746e69e5
ghedger <greg@hedgersoftware.com>
2015-03-18 12:43:12 -0700

    Modularized the system, replacing hardcoded test cases with actual parameters garnered from the input file (like # of samples, frequency).

--- src/misat.cpp
+++ src/misat.cpp
@@ -8,13 +8,30 @@
 void printUsage()
 {
     printf( " \
-HarmPlot\n \
-Harmonic Plotter for AIFF/WAV files\n \
-Copyright (C) 2015 Greg Hedger\n \
-Usage:\n \
-harmplot <file>\n\n" );
-
+            MISAT\n \
+            Musical Instrument Sample Analysis Tool\n \
+            Copyright (C) 2015 Greg Hedger\n \
+            Usage:\n \
+            misat <file>\n\n" );
 }
+
+
+void outputPartials( AudioFile *af, float *pReal, float *pImagOut, float *pRealOut )
+{
+    double step = ( ( double ) af->getPaddedSampleTot() / 2.0 ) / af->getSampleRate();
+    double at = 0;
+    double hz = 0.0;
+    double hzStep = ( af->getSampleRate() / 2 ) / ( double ) af->getPaddedSampleTot() / 2.0;
+    unsigned int i;
+    double end = af->getPaddedSampleTot() / 2.0;
+    while( at < end ) {
+        i = ( unsigned int ) at;
+        printf( "%05.2fHz:\t% 2.4f\n", hz, log(sqrt(pRealOut[i] * pRealOut[i] + pImagOut[i] * pImagOut[i] ) ) );
+        at += 1.0;
+        hz += hzStep * 4;
+    }
+}
+
 
 int main( int argc, const char * argv[])
 {
@@ -22,13 +39,16 @@ int main( int argc, const char * argv[])
         printUsage();
         return -1;
     }
+
     AudioFile *af = new AudioFile();
     if( !af->load( argv[1] ) )
     {
+        // Perform FFT Operation
+
         // Allocate buffers for transform
-        float *pReal = new float[ af->getSampleTot() * 2 ];
-        float *pImagOut = new float[ af->getSampleTot() *2 ];
-        float *pRealOut = new float[ af->getSampleTot() *2 ];
+        float *pReal = new float[ af->getPaddedSampleTot() ];
+        float *pImagOut = new float[ af->getPaddedSampleTot() ];
+        float *pRealOut = new float[ af->getPaddedSampleTot() ];
 
         // Initialize FFT engine
         // GPH NOTE: it calls this internally.
@@ -39,20 +59,7 @@ int main( int argc, const char * argv[])
 
         WindowFunc(6, sampleTot, af->getBuffer() );
         FFT( sampleTot, false, af->getBuffer(), NULL, pImagOut, pRealOut );
-
-        double step = ( ( double ) af->getPaddedSampleTot() / 2.0 ) / af->getSampleRate();
-        double at = 0;
-        double hz = 0.0;
-        double hzStep = ( af->getSampleRate() / 2 ) / ( double ) af->getPaddedSampleTot() / 2.0;
-        unsigned int i;
-        double end = af->getPaddedSampleTot() / 2.0;
-        while( at < end ) {
-            i = ( unsigned int ) at;
-            printf( "%05.2fHz:\t% 2.4f\n", hz, log(sqrt(pRealOut[i] * pRealOut[i] + pImagOut[i] * pImagOut[i] ) ) );
-            at += 1.0;
-            hz += hzStep * 4;
-        }
-
+        outputPartials( af, pReal, pImagOut, pRealOut );
 
         // Clean up
         DeinitFFT();
@@ -62,6 +69,7 @@ int main( int argc, const char * argv[])
     } else {
         cout << "\nTrouble loading file...\n";
     }
+
     delete af;
     return 0;
 }
